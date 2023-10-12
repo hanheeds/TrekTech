60436c8a8f01007b6381791ccdfa5c4b347b1d18
Hanhee Yang <123617674+hanheeds@users.noreply.github.com>
2023-10-12 02:00:00 -0500

    Add files via upload
    Made edits, so that all api keys can be applied.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import openai
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+def load_secrets():
+    """
+    Function to get api keys from .env files. More api key scan be added and returned.
+    """
+    load_dotenv()
+    env_path = Path(".") / ".env"
+    load_dotenv(dotenv_path=env_path)
+
+    open_ai_key = os.getenv("OPENAI_API_KEY")
+
+    return {
+        "OPENAI_API_KEY": open_ai_key
+    }
 
 # Set your OpenAI API key
-api_key = (os.environ.get("OPENAI_KEY"))
+api_key = load_secrets()["OPENAI_API_KEY"]
 
 # Initialize the OpenAI API client
 openai.api_key = api_key
